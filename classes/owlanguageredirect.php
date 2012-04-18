6d2dfffca07f0d9a22fa5f5540dbcbd572cd3a1b
Simon Boyer <simon.boyer@openwide.fr>
2012-04-18 14:49:10 +0200

    OWLanguageRedirect v1.0

--- classes/owlanguageredirect.php
+++ classes/owlanguageredirect.php
@@ -1,7 +1,31 @@
 <?php 
+
+//
+// ## BEGIN COPYRIGHT, LICENSE AND WARRANTY NOTICE ##
+// SOFTWARE NAME: OWLanguageRedirect
+// SOFTWARE RELEASE: 1.0
+// POWERED BY: OpenWide http://www.openwide.fr/
+// SOFTWARE LICENSE: GNU General Public License v2.0
+// NOTICE: >
+//  This program is free software; you can redistribute it and/or
+//  modify it under the terms of version 2.0  of the GNU General
+//  Public License as published by the Free Software Foundation.
+//
+//  This program is distributed in the hope that it will be useful,
+//   but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//  You should have received a copy of version 2.0 of the GNU General
+//  Public License along with this program; if not, write to the Free
+//  Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+//  MA 02110-1301, USA.
+// ## END COPYRIGHT, LICENSE AND WARRANTY NOTICE ##
+//
+
 class owLanguageRedirect {
 
-	protected function redirect(  ) {
+	public function redirect(  ) {
 		// get current locale
 		$ini = eZINI::instance();
 		$currentLocale = $ini->variable('RegionalSettings','ContentObjectLocale');
@@ -17,15 +41,27 @@ class owLanguageRedirect {
 	
 			// redirect if necessary
 			if ( isset( $languageRedirectArray[$userLocale] ) ) {
-				$http = eZHTTPTool::instance();
-				$http->redirect( "/switchlanguage/to/" . $languageRedirectArray[$userLocale] . $_SERVER['REQUEST_URI'] );
-				return false;
-			} else {
-				return false;
+				
+				$requestUri = $_SERVER['REQUEST_URI'];
+				
+				// Get system prefix
+				$sys = eZSys::instance();
+        		$dir = $sys->indexDir();
+				
+        		// Build redirect URL
+        		if ($dir) {
+	        		$requestUri = preg_replace( '!^'.$dir.'/!', '/', $requestUri );
+	        		$requestUri = preg_replace( '!^'.$dir.'$!', '/', $requestUri );
+        		}
+        		$redirect = "/switchlanguage/to/" . $languageRedirectArray[$userLocale] . $requestUri;
+        		
+        		// Redirect
+        		$http = eZHTTPTool::instance();
+        		$http->redirect( $redirect );
+        		
+        		return true;
 			}
-			
 		}
-	
 		return false;
 	}
 }
--- classes/owlanguageredirectswitcher.php
+++ classes/owlanguageredirectswitcher.php
@@ -1,7 +1,29 @@
 <?php 
 
-// Override ezpLanguageSwitcher to save favorite user language (may be different of browser_locale)
+//
+// ## BEGIN COPYRIGHT, LICENSE AND WARRANTY NOTICE ##
+// SOFTWARE NAME: OWLanguageRedirect
+// SOFTWARE RELEASE: 1.0
+// POWERED BY: OpenWide http://www.openwide.fr/
+// SOFTWARE LICENSE: GNU General Public License v2.0
+// NOTICE: >
+//  This program is free software; you can redistribute it and/or
+//  modify it under the terms of version 2.0  of the GNU General
+//  Public License as published by the Free Software Foundation.
+//
+//  This program is distributed in the hope that it will be useful,
+//   but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//  You should have received a copy of version 2.0 of the GNU General
+//  Public License along with this program; if not, write to the Free
+//  Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+//  MA 02110-1301, USA.
+// ## END COPYRIGHT, LICENSE AND WARRANTY NOTICE ##
+//
 
+// Override ezpLanguageSwitcher to save favorite user language (may be different of browser_locale)
 class owLanguageRedirectSwitcher extends ezpLanguageSwitcher
 {
 	
@@ -17,7 +39,6 @@ class owLanguageRedirectSwitcher extends ezpLanguageSwitcher
 		
 		// set cookie to store locale
 		setcookie( 'locale', $locale, time()+60*60*24*30, '/', $domain );
-		
 		
 		// get parent function
 		parent::process();
